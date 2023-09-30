b711eb7ec127701c53371b914fdade74ff6192a1
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-09-30 16:39:01 -0400

    connecting router to app.js

--- config/app.js
+++ config/app.js
@@ -6,6 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('../app/routes/index');
 var usersRouter = require('../app/routes/users');
+var homePageRouter = require('../app/routes/homePage');
+var aboutMeRouter = require('../app/routes/aboutMe');
+var projectsPageRouter = require('../app/routes/projectsPage');
+var servicesPageRouter = require('../app/routes/servicesPage');
+var contactMeRouter = require('../app/routes/contactMe');
+
 
 var app = express();
 
@@ -19,8 +25,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', homePageRouter);
+app.use('/aboutMe', aboutMeRouter);
+app.use('/projectsPage', projectsPageRouter);
+app.use('/servicesPage', servicesPageRouter);
+app.use('/contactMe', contactMeRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
