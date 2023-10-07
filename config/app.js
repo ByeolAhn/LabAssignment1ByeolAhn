108902a269ee41b44a6a25cfbae5c19142f39f48
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-10-07 19:48:41 -0400

    comment edit

--- config/app.js
+++ config/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// use routes from allRouter
+// use routes from indexRouter
 app.use('/', indexRouter);
 
 
